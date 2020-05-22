3e44138eb886d7925d5dbadfb87b98e944991be6
Marianna Sowinska <marianna.sowi@gmail.com>
2020-05-22 20:17:30 +0200

    Add task -6.4 Generowanie listy tytułów-

--- js/script.js
+++ js/script.js
@@ -40,4 +40,59 @@ const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+
+
+// Module 6.4
+
+const optArticleSelector = '.post',
+      optTitleSelector = '.post-title',
+      optTitleListSelector = '.titles';
+      optArticleTagsSelector = '.post-tags .list';
+
+function generateTitleLinks(){
+  console.log('function generateTitleLinks has started')
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector)
+  console.log('znaleziono linki')
+  titleList.innerHTML = '';
+  console.log('wyczyszczono linki')
+
+  /* find all the articles and save them to variable articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+let html = '';
+
+  /* for each article */
+  for(let article of articles){
+
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+        console.log('odczytanie atrybutu ID');
+
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log('odczytano tytul artykulu');
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+
+    /* insert link into titleList */
+    html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
+}
+
+  const links = document.querySelectorAll('.titles a');
+  console.log(links);
+
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
+
+generateTitleLinks();
 }
