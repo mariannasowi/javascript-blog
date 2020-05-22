0da875d1dbf380bb6f708cc41739a8e56b0267b3
Marianna Sowinska <marianna.sowi@gmail.com>
2020-05-22 19:23:26 +0200

    Add task -Blokowanie domyślnej akcji-Zadanie: Dokończenie handlera eventu-

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const titleClickHandler = function(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
 
@@ -16,12 +17,23 @@ const titleClickHandler = function(event){
   clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts article.active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('Clicked selector is:', articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('Chosen article is:', targetArticle);
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+  console.log('Active article is:', targetArticle);
 }
 
 const links = document.querySelectorAll('.titles a');
