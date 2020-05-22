c339a9a78c525e215054477cc1e0360de57dd585
Marianna Sowinska <marianna.sowi@gmail.com>
2020-05-22 19:07:36 +0200

    Change task 1.Pętla for-of 2. Zmiana klas elementu na stronie

--- js/script.js
+++ js/script.js
@@ -1,41 +1,33 @@
 'use strict';
 
-  function titleClickHandler(event){
-  event.preventDefault();
-  const clickedElement = this;
+const titleClickHandler = function(event){
   console.log('Link was clicked!');
 
-  /* [DONE] remove class 'active' from all article links  */
+  /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
+
   for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
+  activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
-  console.log('clickedElement:' + clickedElement);
-  console.log('clickedElement: (with plus)' + clickedElement);
-  clickedElement.classList.add('active');
+  /* add class 'active' to the clicked link */
 
-  /* [DONE] remove class 'active' from all articles */
+  /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
+
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
 
   /* get 'href' attribute from the clicked link */
-  const articleSelector = clickedElement.getAttribute('href');
-  console.log('Clicked selector is:', articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
-  const targetArticle = document.querySelector(articleSelector);
-  console.log('Chosen article is:', targetArticle);
 
   /* add class 'active' to the correct article */
-  targetArticle.classList.add('active');
-  console.log('Active article is:', targetArticle);
+}
 
-  const links = document.querySelectorAll('.titles a');
+const links = document.querySelectorAll('.titles a');
 
-  for(let link of links){
+for(let link of links){
   link.addEventListener('click', titleClickHandler);
-  }
+}
